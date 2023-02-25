4de157c30c22fe2c620b83bfeb4a5db651526147
ocin-effendy <nikoardia134@gmail.com>
2023-02-25 12:33:32 +0700

    init

---
+++ app/src/main/java/com/example/myrecyclerview/Hero.kt
@@ -0,0 +1,9 @@
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Hero(
+    val name: String,
+    val description: String,
+    val photo: Int
+) : Parcelable
